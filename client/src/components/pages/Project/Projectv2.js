48490ea0a70b75ef381b5f38db80474cc21ab100
Johan Cervantes <jccamacho19992@gmail.com>
2020-06-06 19:52:28 -0700

    Aligned the infobar

--- client/src/components/pages/Project/Projectv2.js
+++ client/src/components/pages/Project/Projectv2.js
@@ -1,22 +1,42 @@
+/**
+ * TODOS 1:43AM:
+ *      Add the icons to the site
+ *      make the inforow two columns justified between, with the left column having a row with space-between
+ *      Move from hardcode to server reply.
+ *      Find a background balance.
+ * 
+ */
+
 import React, { Component } from "react";
 import "./Project.css";
 
 // Styling
 import RoundedButton from "../../modules/RoundedButton.js";
 
-const PEOPLE_ICON = "";
-const TIME_ICON = "";
+// Assets 
+import AwardSVG from "../../../assets/icons/mentor.svg";
+import HandSVG from "../../../assets/images/people_needed.svg";
+import TimeSVG from "../../../assets/images/project_timeline.svg";
+import TrophySVG from "../../../assets/images/project_owner.svg";
+import MembersSVG from "../../../assets/icons/members.svg";
+
+const MEMBERS_ICON = (<img src={MembersSVG}/>);
+const MENTOR_ICON = (<img src={AwardSVG}/>);
+const HAND_ICON = (<img src={HandSVG}/>);
+const TIME_ICON = (<img src={TimeSVG}/>);
+const TROPHY_ICON = (<img src={TrophySVG}/>)
 const MOCK_DATA = {
     title: "The Next Big Thing",
     shortDescription: "Join the community of innovative college students, Explore existing projects or post your own for others to join.",
     date: "3",
     imageURL: "https://croud123.wpengine.com/wp-content/uploads/2015/09/landing-page-banner.jpg",
     memberCount: 10,
+    skillsNeeded: ["Breathing", "Existing", "Walking", "Racing"],
 }
 
 const ProjectDetail = (title, text) => {
     return (
-    <div className="col-md-4">
+    <div className="col-md-6">
             <div className="ProjectBox">
                 <div className="ProjectBox-label">
                     {title}
@@ -43,7 +63,6 @@ class Project extends Component {
 
     componentDidMount() {
         let projectID;
-        console.log("Project ID: " + projectID)
     }
 
     getTitleRow = () => {
@@ -62,30 +81,63 @@ class Project extends Component {
             </div>
             
         )
+        
         const infoRow = (
             <div className="container">
 
                 <div className="row justify-content-between " >
+                    
                     <div className="col-md-2 infobarLabel">
-                        People Needed <br />
-                        <div className="infobarInfo">2 Members</div>
+                        <div className="container">
+                            <div className="row">
+                                {HAND_ICON} 
+                                <div>
+                                    People Needed <br />
+                                    <div className="infobarInfo">2 People</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                </div>
                     <div className="col-md-2 infobarLabel">
-                        Project Owner <br />
-                        <div className="infobarInfo">Michael Li</div>
-                </div>
+                        <div className="container">
+                            <div className="row">
+                                {TROPHY_ICON}
+                                <div>
+                                    Project Owner <br />
+                                    <div className="infobarInfo">Michael Li</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="col-md-2 infobarLabel">
-                        Mentor <br />
-                        <div className="infobarInfo">Marzia</div>
-                </div>
+                        <div className="container">
+                            <div className="row">
+                                {MENTOR_ICON}
+                                <div>
+                                    Mentor <br />
+                                    <div className="infobarInfo">Marzia</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="col-md-2 infobarLabel">
-                        Project Timeline <br />
-                        <div className="infobarInfo">1 Month</div>
-                </div>
-                <div className="col-md-2 infoBarLabel">
-                    <RoundedButton label="Join" bgcolor="#13133A" />
-                </div>
+                        <div className="container">
+                            <div className="row">
+                                {TIME_ICON}
+                                <div>
+                                    Project  Timeline<br />
+                                    <div className="infobarInfo">2 Months</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 justify-content-end infoBarLabel ">
+                        
+                        <div className="col-md-6 float-right">
+                            <RoundedButton label="Join" bgcolor="#13133A" />
+                        </div>
+                    </div>
                 </div>
             </div>
         )
@@ -93,12 +145,38 @@ class Project extends Component {
             <div>
                     {mainText}
                     <br/>
+                    
                     <br/>
                     {infoRow}
             </div>
         )
     }
 
+    getExtraDetailBox = () => {
+        let skillsNeeded = this.state.projectData.skillsNeeded;
+        
+        let skillCards = skillsNeeded.map((skill) => {
+            return (<div className="col-md-auto roundedBox">{skill}</div>)
+        });
+
+        return (
+            <div className="container extraDetails">
+                <br/>
+                <h3 className="row">Current Team Size</h3>
+                <div className="row">{MEMBERS_ICON} 6 Members</div>
+                <br/>
+                <h3 className="row">Skills Required</h3>
+                <div className="row">
+                    {skillCards}
+                </div>
+                <br/>
+                <h3 className="row">Resources</h3>
+                <div className="row">
+                    Resource 2
+                </div>
+            </div>
+        )
+    }
 
     getProjectDetails = () => {
         let details = [
@@ -111,7 +189,14 @@ class Project extends Component {
             <div className="container">
 
                 <div className="row">
-                        {details.map((detail) => ProjectDetail(detail[0], detail[1]))}
+                        <div className="col-md-3">
+                            {this.getExtraDetailBox()}
+                        </div>
+                        <div className="col-md-9">
+                            <div className="row">
+                                {details.map((detail) => ProjectDetail(detail[0], detail[1]))}
+                            </div>
+                        </div>
                 </div>
             </div>
         )
@@ -123,10 +208,14 @@ class Project extends Component {
         const projectDetails = this.getProjectDetails();
 
         return (
-            <div className="project">
-                {titleRow}
-                {projectDetails}
-            </div>
+            <>
+                <div className="project">
+                    {titleRow}
+                </div>
+                <div className="greybg">
+                    {projectDetails}
+                </div>
+            </>
         )
     }
 }
